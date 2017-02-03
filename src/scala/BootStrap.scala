86433d1fa10143b9abb7cd3e9acbd5939e716013
christen <hiczx@qq.com>
2017-02-03 20:46:20 +0800

    add code

--- src/scala/BootStrap.scala
+++ src/scala/BootStrap.scala
@@ -3,10 +3,19 @@
   */
 object BootStrap {
   def main(args: Array[String]) {
+
     val name = "Christen, She's just not that into you Dear"
     println(name.toUpperCase())
+
     val bonjour = "Hello" :: "Hola" :: "Guten Tag" :: "こんにちは" :: "您好" :: Nil
     println(bonjour.exists(s => s == "Hola"))
+
+    def factorial(x: BigInt): BigInt = (BigInt(1) to x).reduce(_ * _)
+    println(factorial(4))
+
+    var capital = Map("US" -> "Washington", "France" -> "Paris", "Japan" -> "Tokyo")
+    capital += ("España" -> "Madrid")
+    println(capital("España"))
   }
 }
 
