e7bc28a0b3e86d7c042e3fc469ab644499040bf5
chenzixin <18610089982@163.com>
2017-02-03 18:40:04 +0800

    First

--- src/hello/Main.scala
+++ src/hello/Main.scala
@@ -5,7 +5,7 @@ package hello
   */
 object Hello {
   def main(args: Array[String]) {
-    val name = "Christen"
+    val name = "Christen, She's just not that into you Dear"
     println(name.toUpperCase())
     val bonjour = "Hello" :: "Hola" :: "Guten Tag" :: "こんにちは" :: "您好" :: Nil
     println(bonjour.exists(s => s == "Hola"))
