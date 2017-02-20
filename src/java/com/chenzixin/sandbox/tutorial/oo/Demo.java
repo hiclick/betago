b411d29427f12e4e3c11ee0e4b53f60ebc8d7749
christen <hiczx@qq.com>
2017-02-20 14:49:59 +0800

    update sructure

---
+++ src/java/com/chenzixin/sandbox/tutorial/oo/Demo.java
@@ -0,0 +1,7 @@
+package com.chenzixin.sandbox.tutorial.oo;
+
+/**
+ * Created by christen on 2/20/2017.
+ */
+public class Demo {
+}
---
+++ src/scala/src/com/chenzixin/book/packt/Demo.scala
@@ -0,0 +1,8 @@
+package com.chenzixin.book.packt
+
+/**
+  * Created by christen on 2/20/2017.
+  */
+class Demo {
+
+}
